d11b40bc5c3360fc62171b940be1b960bb196eb9
NoSurprises <helloma@yandex.ru>
2017-11-27 13:46:06 +0300

    recipe view as image with text

--- app/src/main/java/antitelegram/devenirchef/MainActivity.java
+++ app/src/main/java/antitelegram/devenirchef/MainActivity.java
@@ -1,7 +1,13 @@
 package antitelegram.devenirchef;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("nic");
+
+
+        int[] tempDrawables = {R.drawable.burger,R.drawable.fish,R.drawable.pancakes,R.drawable.pumpkins,R.drawable.soup };
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.recipes_linear_layout);
+        for (int i = 0; i < 5; i++) {
+            View view = getLayoutInflater().inflate(R.layout.recipe_main_screen, linearLayout, false);
+            linearLayout.addView(view);
+
+
+            ((ImageView) view.findViewById(R.id.recipe_image)).setImageDrawable(ResourcesCompat.getDrawable(
+                    getResources(), tempDrawables[i], null)
+            );
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
