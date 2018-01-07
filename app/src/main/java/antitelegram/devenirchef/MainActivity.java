a3d127d3971ffc62b5f8c6a9397135f85a59bb06
NoSurprises <helloma@yandex.ru>
2018-01-07 14:52:55 +0300

    added navigation drawer

--- app/src/main/java/antitelegram/devenirchef/MainActivity.java
+++ app/src/main/java/antitelegram/devenirchef/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -45,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         recipesLayout = findViewById(R.id.recipes_linear_layout);
         recipes = new ArrayList<>();
 
@@ -194,9 +198,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.options_menu, menu);
-        return true;
-    }
+
 }
