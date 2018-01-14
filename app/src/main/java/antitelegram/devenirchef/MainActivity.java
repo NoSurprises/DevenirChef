3a8da9d37dcd189d1277f62c6ac8c5d9c8152429
nosurprises <helloma@yandex.ru>
2018-01-14 13:28:36 +0300

    DC-23 create animated toggle for nav drawer

--- app/src/main/java/antitelegram/devenirchef/MainActivity.java
+++ app/src/main/java/antitelegram/devenirchef/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
@@ -137,6 +138,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpToolbar() {
         initToolbar();
+        setUpDrawerToggle();
+
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -145,6 +148,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void setUpDrawerToggle() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     private void removeAllRecipes() {
