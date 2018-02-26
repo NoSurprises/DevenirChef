3a7748d2d533c6b944f2c1eba177eb6189b29fcb
agnik1998 <anikolayshvili@gmail.com>
2018-02-26 23:16:45 +0300

    Fixed tag menu Fixed search submit button

--- app/src/main/java/antitelegram/devenirchef/MainActivity.java
+++ app/src/main/java/antitelegram/devenirchef/MainActivity.java
@@ -169,8 +169,6 @@ public class MainActivity extends DrawerBaseActivity {
                     collapse(bottomMenuExpanded, bottomMenuShrinked.getHeight(), bottomMenuShrinked);
                     setShrinkedComplexity();
                     setShrinkedTags();
-                    selectRecipesComplexityAndTags();
-
                 } else {
                     expand(bottomMenuExpanded, bottomMenuShrinked.getHeight(), bottomMenuShrinked);
                 }
@@ -288,8 +286,6 @@ public class MainActivity extends DrawerBaseActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                recipesAdapter.changeDataset(searchFilter(query));
-
                 return false;
             }
 
