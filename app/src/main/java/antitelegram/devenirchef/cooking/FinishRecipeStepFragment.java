a6b84053b9456d9237937ba9c4291568ecc831e3
NoSurprises <helloma@yandex.ru>
2018-02-26 20:34:36 +0300

    Bind image buttons

--- app/src/main/java/antitelegram/devenirchef/cooking/FinishRecipeStepFragment.java
+++ app/src/main/java/antitelegram/devenirchef/cooking/FinishRecipeStepFragment.java
@@ -44,11 +44,11 @@ public class FinishRecipeStepFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final String TAG = "daywint";
     private Button takePhoto;
-    private Button mainScreen;
+    private View mainScreen;
     private ImageView imageView;
     private Bitmap finishImage;
     private String takenImagePath;
-    private Button share;
+    private View share;
     private Uri photoUri;
     private PhotoRedactor redactor;
 
